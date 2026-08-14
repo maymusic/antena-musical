e0e53fc040b1ec9ab44c74693c736326494ac374
maymusic <maymusicmexico@gmail.com>
2026-08-13 23:02:38 -0600

    Delete lib directory

--- lib/crypto.ts
+++
@@ -1,9 +0,0 @@
-import { createHash, randomBytes } from "crypto";
-
-export function createOpaqueToken() {
-  return randomBytes(32).toString("hex");
-}
-
-export function hashOpaqueToken(token: string) {
-  return createHash("sha256").update(token).digest("hex");
-}
